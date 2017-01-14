ac6cc4b1bd38b3c37c4918cbb6d88a5b946e81d8
Alexander Dushkin <ads2alpha@gmail.com>
2017-01-14 20:54:52 +0200

    Initial commit

---
+++ route.py
@@ -0,0 +1,36 @@
+from random import SystemRandom
+from string import digits
+from flask import Flask, request, session, render_template
+
+app = Flask(__name__)
+app.secret_key = ''.join(SystemRandom().choice(digits) for _ in range(32))
+
+available_languages = ['en', 'fr', 'ro', 'ru']
+
+def get_language(headers):
+    """
+    Detect language using the HTTP Accept-Language header.
+    """
+    try:
+        lang = session['reinproject-locale']
+    except:
+        lang = headers.get('Accept-Language')[:2]
+        if lang not in available_languages:
+            lang = 'en'
+        session['reinproject-locale'] = lang
+    return lang
+
+def display(headers, template_name):
+    """
+    Wrapper around render_template for displaying localized page.
+    """
+    lang = get_language(headers)
+    return render_template(lang + '/' + template_name)
+
+@app.route('/')
+def index():
+    """Index page."""
+    return display(request.headers, 'index.html')
+
+if __name__ == '__main__':
+    app.run()

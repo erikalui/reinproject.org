ef6147d942723b6ba0e8fed419cb0f2138e3d52b
David Sterry <davids@exchb.com>
2017-01-28 05:13:11 +0000

    always select lang on request locale (set back when we can link), better title for en

--- app/route.py
+++ app/route.py
@@ -5,19 +5,19 @@ from flask import Flask, request, session, render_template
 application = Flask(__name__)
 application.secret_key = ''.join(SystemRandom().choice(digits) for _ in range(32))
 
-available_languages = ['en', 'fr', 'ro', 'ru']
+available_languages = ['en', 'lv', 'hi', 'tl', 'ru']
 
 def get_language(headers):
     """
     Detect language using the HTTP Accept-Language header.
     """
-    try:
-        lang = session['reinproject-locale']
-    except:
-        lang = headers.get('Accept-Language')[:2]
-        if lang not in available_languages:
-            lang = 'en'
-        session['reinproject-locale'] = lang
+    #try:
+        #lang = session['reinproject-locale']
+    #except:
+    lang = headers.get('Accept-Language')[:2]
+    if lang not in available_languages:
+        lang = 'en'
+    session['reinproject-locale'] = lang
     return lang
 
 def display(headers, template_name):

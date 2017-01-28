6437d8f38dc29f19ddc653d869ee8a21fc388f70
David Sterry <davids@exchb.com>
2017-01-28 09:33:56 +0000

    allow user to select locale via dropdown

--- app/route.py
+++ app/route.py
@@ -1,6 +1,6 @@
 from random import SystemRandom
 from string import digits
-from flask import Flask, request, session, render_template
+from flask import Flask, request, redirect, session, render_template
 
 application = Flask(__name__)
 application.secret_key = ''.join(SystemRandom().choice(digits) for _ in range(32))
@@ -11,13 +11,13 @@ def get_language(headers):
     """
     Detect language using the HTTP Accept-Language header.
     """
-    #try:
-        #lang = session['reinproject-locale']
-    #except:
-    lang = headers.get('Accept-Language')[:2]
-    if lang not in available_languages:
-        lang = 'en'
-    session['reinproject-locale'] = lang
+    try:
+        lang = session['reinproject-locale']
+    except:
+        lang = headers.get('Accept-Language')[:2]
+        if lang not in available_languages:
+            lang = 'en'
+        session['reinproject-locale'] = lang
     return lang
 
 def display(headers, template_name):
@@ -31,6 +31,14 @@ def display(headers, template_name):
 def index():
     """Index page."""
     return display(request.headers, 'index.html')
+
+@application.route('/<lang>')
+def set_language(lang):
+    """Choose language"""
+    if lang in available_languages:
+        session['reinproject-locale'] = lang
+    return display(request.headers, 'index.html')
+
 
 if __name__ == '__main__':
     application.run()
